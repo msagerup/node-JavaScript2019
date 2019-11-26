ad3e7a0fae0b58eef8539ad9e17121cf9a591639
Magnus Sagerup <magnus.sagerup@gmail.com>
2019-11-26 16:34:09 +0100

    getting correct user

--- routes/userRutes.js
+++ routes/userRutes.js
@@ -1,24 +1,79 @@
-// const userRutes = (app, fs) => {
-//     const dataSetPath = './routes/users.json';
+const userRutes = (app, fs) => {
+    const dataSetPath = './routes/users.json';
 
-//     // Get all users
+    // Get all users
 
-//     app.get('/users/:id', (req, res) => {
-//         fs.readFile(dataSetPath, (err, data) => {
-//             if (err) {
-//                 throw new Error('Could not find file');
-//             }
-//             res.send(JSON.parse(data)[req.params.id]);
-//         });
-//     });
-// };
+    app.get('/users/:id', (req, res) => {
+        fs.readFile(dataSetPath, (err, data) => {
+            if (err) {
+                throw new Error('Could not find file');
+            }
 
-// module.exports = userRutes;
+            let userInput = req.params.id;
+            // Store userid
+            let sendUser
+            // Parse data
+            let data2 = JSON.parse(data) 
+            // Search data obj return user
+            data2.forEach(function (user) {
+                if(user.id == userInput) {
+                    sendUser = user
+                }
+            })
+            console.log(sendUser)
+
+            res.send(sendUser);
+        });
+    });
+};
+
+module.exports = userRutes;
 
 
-const data = require('./users.json')
+
+let myjason = `[
+    {
+      "id": "1",
+      "employee_name": "Scott",
+      "employee_salary": "12322",
+      "employee_age": "23",
+      "profile_image": ""
+    },
+    {
+      "id": "6",
+      "employee_name": "[S]Miko",
+      "employee_salary": "2435",
+      "employee_age": "33",
+      "profile_image": ""
+    },
+    {
+      "id": "7",
+      "employee_name": "Jonn Wick 3",
+      "employee_salary": "6000",
+      "employee_age": "31",
+      "profile_image": ""
+    },
+    {
+      "id": "10",
+      "employee_name": "[S]Momo",
+      "employee_salary": "3525",
+      "employee_age": "33",
+      "profile_image": ""
+    }
+]`
+
+let data2 = JSON.parse(myjason) 
 
 
 
 
-console.log(data[0])
+let input = 6
+console.log(data2)
+let blabla
+let restult; 
+data2.forEach(function (user) {
+    if(user.id == input) {
+        restult = user
+    }
+}) //?
+restult 
---
+++ server.js
@@ -0,0 +1,40 @@
+// Create your routes. Use the documentation for express if you don't remember how to do it (https://expressjs.com/en/guide/routing.html) (Lenker til en ekstern side.)
+
+// (GET) "/user/<user_id>"
+// (POST) "/user/"
+// (PUT) "user/<user_id>"
+// (DELETE) "/user/<user_id>"
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+//////////
+// JSON data
+
+const app = express();
+
+//////////
+// FS
+
+const fs = require('fs');
+
+////////
+// Middleware
+
+app.use(bodyParser.json());
+// app.use((req,res,next) => {
+//     console.log('Recieved new request')
+//     next()
+// })
+
+///////
+// Rutes
+
+const rutes = require('./routes/rutes')(app, fs);
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server listening to port: ${port}`);
+});
